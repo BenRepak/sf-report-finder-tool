77c114da170f37cd0142645dba7c0ffe3e9486de
Ben Repak <benrepak@gmail.com>
2022-09-26 21:32:59 -0600

    cleaned the reportFinderListItemCard lwc

--- force-app/main/default/lwc/reportFinderListItemCard/reportFinderListItemCard.js
+++ force-app/main/default/lwc/reportFinderListItemCard/reportFinderListItemCard.js
@@ -1,26 +1,11 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ReportFinderListItemCard extends LightningElement {
-
+    // variable passed from the parent component
     @api
     report;
 
-
-    get reportUrl(){
-        return this.report.accessUrl;
-    }
-    get reportName(){
-        return this.report.name;
-    }
-
-    get reportDescriptionLong() {
-        return this.report.longDescription;
-    }
-
-    get reportType(){
-        return this.report.type;
-    }
-
+    // only shows card if a report is selected
     get hasSelectedReport(){
         console.log('report');
         if(this.report != undefined) {
@@ -30,15 +15,28 @@ export default class ReportFinderListItemCard extends LightningElement {
         }
     }
 
-    get thumbnail(){
-        if(this.report.thumbnail != undefined){
-            return this.report.thumbnail;
-        } else {
-            return '';
-        }
+    // dynamically returns accessUrl
+    get reportUrl(){
+        return this.report.accessUrl;
     }
 
+    // dynamically returns name
+    get reportName(){
+        return this.report.name;
+    }
 
+    // dynamically returns long descriptoin
+    get reportDescriptionLong() {
+        return this.report.longDescription;
+    }
+
+    // dynamically returns report type
+    get reportType(){
+        return this.report.type;
+    }
+
+    // handles event sent from reportFinderBookmarker
+    // passes to reportFinderSelectedItemModal --> reportFinderContainer
     handleBookmarkChange(event){
         console.log('handleBookmarkChange in listItemCard');
         const updatedBookmarkEvent = new CustomEvent('changebookmark', {detail : event.detail});
@@ -46,6 +44,9 @@ export default class ReportFinderListItemCard extends LightningElement {
     }
 
 
+    // opens accessURL in a new tab
+    // dispatches openReportEvent to create a Report_Finder_Item_Usage__c record
+    // event is sent to reportFinderSelectedItemModal --> reportFinderContainer
     handleReportOpen(){
         console.log('handleReportOpen in listItemCard');
         const openReportEvent = new CustomEvent('openreport', {detail: {report : this.report}});

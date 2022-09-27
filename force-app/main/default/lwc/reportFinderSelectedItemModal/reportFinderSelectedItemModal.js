b15c35b89b4a4407822a6e10469c1d1e21af769f
Ben Repak <benrepak@gmail.com>
2022-09-26 21:42:15 -0600

    cleaned the reportFinderSelectedItemModal lwc

--- force-app/main/default/lwc/reportFinderSelectedItemModal/reportFinderSelectedItemModal.js
+++ force-app/main/default/lwc/reportFinderSelectedItemModal/reportFinderSelectedItemModal.js
@@ -1,45 +1,56 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ReportFinderSelectedItemModal extends LightningElement {
-
+    // variable passed from parent component
     @api
     report; 
     
-    openModal = false;
-    isLoaded = false;
+    openModal = false; // shows or hides modal
+    isLoaded = false; // shows or hides loading spinner
     
+    // function called from parent component (reportFinderContainer) when a report item is selected
+    // reportFinderListItem --> reportFinderList --> reportFinderContainer --> reportFinderSelectedItemModal
     @api
     showModal() {
-        console.log('show modal');
+        // console.log('show modal');
         this.openModal = true;
         this.isLoaded = true;
     }
-    closeModal() {
-        console.log('close modal');
-        this.openModal = false;
-       
+
+    // function called from parent component (reportFinderContainer) after a bookmark event has been saved
+    @api
+    closeSpinner(){
+        this.isLoaded = true;
     }
 
+    // closes the modal when the X is clicked in the top right of the header
+    closeModal() {
+        // console.log('close modal');
+        this.openModal = false;
+    }
+
+    // sends changebookmark event to reportFinderContainer where apex is executed
+    // originally called from reportFinderBookmarker --> reportFinderListItemCard
     handleBookmarkChange(event){
         this.isLoaded = false;
-        console.log('handleBookmarkChange in itemModal');
-        console.log('event.detail.addBookmark --> ' + event.detail.addBookmark);
+        // console.log('handleBookmarkChange in itemModal');
+        // console.log('event.detail.addBookmark --> ' + event.detail.addBookmark);
         const updatedBookmarkEvent = new CustomEvent('changebookmark', {detail : event.detail});
         this.dispatchEvent(updatedBookmarkEvent);
 
     }
 
+    // sends openreport event to reportFinderContainer where apex is executed
+    // originally called from reportFinderListItemCard
     handleReportOpen(event){
-        console.log('handleReportOpen in itemModal');
-        console.log(JSON.stringify(event.detail));
+        // console.log('handleReportOpen in itemModal');
+        // console.log(JSON.stringify(event.detail));
         const openReportEvent = new CustomEvent('openreport', {detail : event.detail});
         this.dispatchEvent(openReportEvent);
     }
 
-    @api
-    closeSpinner(){
-        this.isLoaded = true;
-    }
+        
+  
 
 
 

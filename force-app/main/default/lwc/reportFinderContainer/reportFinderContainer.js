cd1b60e4416d6d61f366d33f4b049bbcc4b81172
Ben Repak <benrepak@gmail.com>
2022-09-26 22:40:42 -0600

    cleaned reportFinderContainer lwc

--- force-app/main/default/lwc/reportFinderContainer/reportFinderContainer.js
+++ force-app/main/default/lwc/reportFinderContainer/reportFinderContainer.js
@@ -11,6 +11,8 @@ export default class ReportFinderContainer extends LightningElement {
     @api isLoaded = false;
 
     /** Configure hide/show filters from UI */
+    // set in the config app builder
+    // passed down to the reportFinderFilters component
     @api showJobFunctionFilter;
     @api showPurposeFilter;
     @api showFormatFilter;
@@ -18,36 +20,32 @@ export default class ReportFinderContainer extends LightningElement {
     @api showSearchByNameFilter;
 
     /** Message to display when no reports are found when searching */
+    // set in the config app builder
     @api noReportsMessage;
 
 
     /** Current page in the product list. */
     pageNumber = 1;
     
-    /** The number of items on a page. */
-    pageSize;
-    
-    /** The total number of items matching the selection. */
-    totalItemCount = 0;
-    
     /** JSON.stringified version of filters to pass to apex */
     filters;
     
-    reports;
-    error;
+    reports; // results returned from imperative apex call
+    error; // errors returned from imperative apex call
 
-    selectedReport;
+    selectedReport; // used to pass selected report from reportFinderListItem --> reportFinderList --> reportFinderContainer --> reportFinderSelectedItemModal
 
-    delayTimeout;
+    delayTimeout; // used to delay spinner
     
 
+    // queries reports when the component is loaded
     connectedCallback(){
         console.log('connectedCallback');
         this.updateReports();
     }
 
 
-
+    // retreives search results based on current page number and filters 
     updateReports(){
       refreshReports({filters: this.filters, pageNumber: this.pageNumber }) 
           .then(result => {
@@ -55,20 +53,6 @@ export default class ReportFinderContainer extends LightningElement {
               this.reports = result;
               this.error = undefined;
               console.log('refreshReports');
-            //   if(this.selectedReport !== undefined){
-            //     console.log('entered');
-            //     console.log('selectedReport --> ' + JSON.stringify(this.selectedReport));
-            //     let selectedReportUpdated = this.reports.records.find(r => r.id === this.selectedReport.id);
-            //     if(selectedReportUpdated === undefined){
-            //         selectedReportUpdated = this.selectedReport;
-            //     }
-            //     console.log('SHOW ID selectedReportUpdated -->' + JSON.stringify(selectedReportUpdated));
-            //     console.log('SHOW ID this.selectedReport.id -->' + JSON.stringify(this.selectedReport.id));
-            //     console.log('SHOW ID selectedReportUpdated.id -->' + JSON.stringify(selectedReportUpdated.id));
-            //     console.log('SHOW REPORT selectedReportUpdated -->' + JSON.stringify(selectedReportUpdated));
-            //     console.log('SHOW REPORT  this.selectedReport after2 -->' + JSON.stringify(this.selectedReport));
-            //     this.selectedReport = selectedReportUpdated;
-            //   }
               this.hideSpinner();
           })
           .catch(error => {
@@ -77,11 +61,12 @@ export default class ReportFinderContainer extends LightningElement {
             this.error = JSON.stringify(error);
             this.reports = undefined;
             this.hideSpinner();
-
           });
     }
     
-
+    // handles the filter event from the reportFinderFilters lwc
+    // executes apex search including filter values from child component
+    // reportFinderFilters --> reportFinderContainer
     filteredEvent(event){
         this.pageNumber = 1;
         this.selectedReport = undefined;
@@ -93,6 +78,9 @@ export default class ReportFinderContainer extends LightningElement {
         this.updateReports();
     }
 
+
+    // opens the reportFinderSelectedItemModal lwc when a reportFinderListItem is clicked
+    // reportFinderListItem --> reportFinderList --> reportFinderContainer --> reportFinderSelectedItemModal
     selectedEvent(event){
         console.log('selectedEvent in contatiner');
         this.selectedReport = event.detail;
@@ -103,15 +91,19 @@ export default class ReportFinderContainer extends LightningElement {
     }
 
 
+    // handles the previous event from the reportFinderPaginator lwc
+    // reportFinderPaginator --> reportFinderList --> reportFinderContainer
+    // requeries data with an updated offset
     handlePreviousPage() {
         this.selectedReport = undefined;
         this.showSpinner();
         this.pageNumber = this.pageNumber - 1;
         this.updateReports();
-
-
     }
 
+    // handles the next event from the reportFinderPaginator lwc
+    // reportFinderPaginator --> reportFinderList --> reportFinderContainer
+    // requeries data with an updated offset
     handleNextPage() {
         this.selectedReport = undefined;
         this.showSpinner();
@@ -121,6 +113,10 @@ export default class ReportFinderContainer extends LightningElement {
 
 
 
+    // bookmarks report event from the reportFinderListeItemCard lwc
+    // reportFinderListeItemCard --> reportFinderSelectedItemModal --> reportFinderContainer
+    // when done, the report list is refreshed to show updated bookmarks (updateReports())
+    // also when done, the selectedReport item is updated with a freshly queried report_finder_item__c record to include the updated isBookmarked boolean
     handleBookmarkChange(event){
         console.log('handleBookmarkChange in container');
         console.log('event.detail.addBookmark --> ' + event.detail.addBookmark);
@@ -130,7 +126,7 @@ export default class ReportFinderContainer extends LightningElement {
         updateBookmark({reportId : reportId, isAdd : addBookmark }) 
             .then(result => {
                 console.log('bookmark Result --> ' + JSON.stringify(result));
-                this.selectedReport = result;
+                this.selectedReport = result; // result selectedReport with updated data from apex 
                 this.updateReports();
                 const modalComp =  this.template.querySelector('c-report-finder-selected-item-modal');
                 modalComp.closeSpinner();
@@ -142,13 +138,15 @@ export default class ReportFinderContainer extends LightningElement {
     }
 
 
+    // logs report open event from the reportFinderListeItemCard lwc
+    // reportFinderListeItemCard --> reportFinderSelectedItemModal --> reportFinderContainer
     handleReportOpen(event){
         console.log('handleReportOpen in container');
-        console.log(JSON.stringify(event.detail));
+        // console.log(JSON.stringify(event.detail));
         let reportId = event.detail.report.id;
         logItemUsage({reportId : reportId }) 
             .then(result => {
-                console.log('sucessfully loged report open');
+                console.log('sucessfully logged report open');
             })
             .catch(error => {
                console.log('error --> ' + JSON.stringify(error));
@@ -158,21 +156,21 @@ export default class ReportFinderContainer extends LightningElement {
     }
 
 
+    // shows loading spinner with a slight time delay
     showSpinner(){
         window.clearTimeout(this.delayTimeout);
         // eslint-disable-next-line @lwc/lwc/no-async-operation
         this.delayTimeout = setTimeout(() => {
-
             this.isLoaded = false;
             console.log('showSpinner');
-
         }, DELAY);
     }
 
+
+    // hides loading spinner and clears timeout
     hideSpinner(){
         this.isLoaded = true;
-        clearTimeout(this.delayTimeout);
-        
+        clearTimeout(this.delayTimeout);  
     }
 
 
